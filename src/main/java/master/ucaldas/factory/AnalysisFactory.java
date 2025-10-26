e76a2f12cbd3ac7b5fa56d831fd35ee6e3a46792
Leandro Panesso Atehortúa <leandro@queo.com.co>
2025-10-25 19:16:00 -0500

    feat: :sparkles: Implement AnalysisFactory and AnalysisType for dynamic analysis strategy creation

---
+++ src/main/java/master/ucaldas/factory/AnalysisFactory.java
@@ -0,0 +1,35 @@
+package master.ucaldas.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import master.ucaldas.strategy.AlignmentAnalysis;
+import master.ucaldas.strategy.AnalysisStrategy;
+import master.ucaldas.strategy.MotifDetectionAnalysis;
+import master.ucaldas.strategy.StructurePredictionAnalysis;
+
+public class AnalysisFactory {
+    private final Map<AnalysisType, Supplier<AnalysisStrategy>> strategies = new HashMap<>();
+
+    public AnalysisFactory() {
+        strategies.put(AnalysisType.ALIGNMENT, AlignmentAnalysis::new);
+        strategies.put(AnalysisType.STRUCTURE_PREDICTION, StructurePredictionAnalysis::new);
+    }
+
+    public AnalysisStrategy createAnalysis(AnalysisType type, String... params) {
+        if (type == AnalysisType.MOTIF_DETECTION) {
+            if (params.length == 0 || params[0] == null || params[0].isEmpty()) {
+                throw new IllegalArgumentException("Se requiere un motivo para detección");
+            }
+            return new MotifDetectionAnalysis(params[0]);
+        }
+
+        Supplier<AnalysisStrategy> supplier = strategies.get(type);
+        if (supplier == null) {
+            throw new IllegalArgumentException("Tipo de análisis no soportado: " + type);
+        }
+        
+        return supplier.get();
+    }
+}
---
+++ src/main/java/master/ucaldas/factory/AnalysisType.java
@@ -0,0 +1,8 @@
+package master.ucaldas.factory;
+
+public enum AnalysisType {
+    ALIGNMENT,
+    MOTIF_DETECTION,
+    STRUCTURE_PREDICTION
+}
+
